23e674b341ebcd67ac3905cb3648ddbf45f4dea1
Reetta Puska <reetta.puska@gmail.com>
2018-11-11 14:51:33 +0200

    fixed an issue to test codacy

--- src/main/java/ohtu/main/Main.java
+++ src/main/java/ohtu/main/Main.java
@@ -5,6 +5,8 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-
+        int storageCapacity = 1;
+        Varasto varasto = new Varasto(storageCapacity);
+        System.out.println(varasto);
     }
 }
